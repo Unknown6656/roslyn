47c64993f4f4546646ecbe345e11993d0ef9f018
Matthew Windsor <t-mawind@microsoft.com>
2016-05-27 17:53:21 +0100

    Add instances to example.

--- concepts/tests/101.cs
+++ concepts/tests/101.cs
@@ -2,18 +2,27 @@
 using System;
 
 class ConceptAttribute : System.Attribute {}
+class ConceptInstanceAttribute : System.Attribute {}
 
 interface I { 
 }
 
-concept J {
+concept J<T> {
 
 }
 
 [Concept]
-interface J2
+interface J2<T>
 {
 
+}
+
+instance JInt : J<int> {
+}
+
+[ConceptInstance]
+struct J2Int : J2<int>
+{
 }
 
 class Program {
