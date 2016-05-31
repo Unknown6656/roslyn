16d3fcbd885bb6e9b53f0065cc0dc79c1aeba854
Matthew Windsor <t-mawind@microsoft.com>
2016-05-31 13:00:00 +0100

    Expand 101.cs a bit more.

--- concepts/tests/101.cs
+++ concepts/tests/101.cs
@@ -4,30 +4,76 @@ using System;
 class ConceptAttribute : System.Attribute {}
 class ConceptInstanceAttribute : System.Attribute {}
 
-interface I { 
+//
+// Normal interface and struct.
+//
+// These shouldn't be modified.
+//
+
+interface I<T>
+{
+} 
+
+struct IInt : I<int>
+{
 }
 
-concept J<T> {
+//
+// Concepts.
+//
+// These should be converted down into interfaces and structs.
+//
 
+concept J<T>
+{
 }
+
+instance JInt : J<int>
+{
+}
+
+
+//
+// 'Separate compilation' desugared concepts.
+//
+// These should be unmodified, but work as if they were concepts.
+//
 
 [Concept]
 interface J2<T>
 {
-
 }
 
-instance JInt : J<int> {
-}
 
 [ConceptInstance]
 struct J2Int : J2<int>
 {
 }
 
+//
+// 'Over-annotated' concepts.
+//
+// These should not have the annotations added twice.
+//
+
+[Concept]
+concept J3<T>
+{
+}
+
+
+[ConceptInstance]
+instance J3Int : J3<int>
+{
+}
+
+//
+// Driver.
+//
+
 class Program {
-   static void Main() {
+   static void Main()
+   {
    }
-  
 }
 
