906071dfc6868278a5fc2bb44a74fcb5f8133e38
Matthew Windsor <t-mawind@microsoft.com>
2016-05-31 13:32:23 +0100

    Add Eq playpens.

---
+++ concepts/tests/eq_stage2.cs
@@ -0,0 +1,50 @@
+// Fully expressed concept-based Eq.
+
+class ConceptAttribute : System.Attribute {}
+class ConceptInstanceAttribute : System.Attribute {}
+
+concept Eq<A>
+{
+    bool Equals(A a, A b);
+}
+
+instance EqInt : Eq<int>
+{
+    public bool Equals(int a, int b) => a == b;
+}
+
+instance EqArray<EqA,A> : Eq<A[]> where EqA: struct, Eq<A>
+{
+    public bool Equals(A[] a, A[] b)
+    {
+        var dict = default(EqA);
+        if (a == null) return b == null;
+        if (b == null) return false;
+        if (a.Length != b.Length) return false;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (!dict.Equals(a[i], b[i])) return false;
+        }
+        return true;
+    }
+}
+
+//
+// Driver.
+//
+
+class Program {
+   static void Main()
+   {
+        var dict = default(EqArray<EqInt, int>);
+        System.Console.Out.Write("1: ");
+        System.Console.Out.WriteLine(dict.Equals(new int[] {}, new int[] {}) ? "pass" : "fail");
+        System.Console.Out.Write("2: ");
+        System.Console.Out.WriteLine(dict.Equals(new int[] {  1, 2, 3 }, new int[] { 1, 2, 3 }) ? "pass" : "fail");
+        System.Console.Out.Write("3: ");
+        System.Console.Out.WriteLine(dict.Equals(new int[] { 1, 2, 3 }, new int[] { 1, 2 }) ? "fail" : "pass");
+        System.Console.Out.Write("4: ");
+        System.Console.Out.WriteLine(dict.Equals(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }) ? "fail" : "pass");
+    }
+}
+
