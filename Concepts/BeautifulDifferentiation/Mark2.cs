f6254d3c23084528ada68131ed54ba7a9682101e
Matthew Windsor <t-mawind@microsoft.com>
2016-06-22 14:57:42 +0100

    Separate out code in BD examples.

---
+++ Concepts/BeautifulDifferentiation/Mark2.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Concepts.Prelude;
+
+/// <summary>
+///     Mark 2 beautiful differentiation.
+/// </summary>
+namespace BeautifulDifferentiation.Mark2
+{
+    using FuncInstances;
+    using static NumUtils;
+
+    instance NumDA<A> : Num<D<A>>
+        where NumA : Num<A>
+    {
+        D<A> FromInteger(int x) => D<A>.Const(FromInteger(x));
+
+        D<A> Add(D<A> x, D<A> y)
+            => new D<A>(Add(x.X, y.X), Add(x.DX, y.DX));
+
+        D<A> Mul(D<A> x, D<A> y)
+            // Product rule
+            => new D<A>(Mul(x.X, y.X), Add(Mul(x.DX, y.X), Mul(y.DX, x.X)));
+
+        D<A> Sub(D<A> x, D<A> y)
+            => new D<A>(Sub(x.X, y.X), Sub(x.DX, y.DX));
+
+        D<A> Signum(D<A> x) =>
+            D<A>.Chain(Signum, NumF<A, A, NumA>.FromInteger(0))(x);
+
+        D<A> Abs(D<A> x) => D<A>.Chain(Abs, Signum)(x);
+    }
+
+    instance FractionalDA<A> : Fractional<D<A>>
+        where FracA : Fractional<A>
+    {
+        // Implementation of Num
+        D<A> FromInteger(int x)  => NumDA<A, FracA>.FromInteger(x);
+        D<A> Add(D<A> x, D<A> y) => NumDA<A, FracA>.Add(x, y);
+        D<A> Mul(D<A> x, D<A> y) => NumDA<A, FracA>.Mul(x, y);
+        D<A> Sub(D<A> x, D<A> y) => NumDA<A, FracA>.Sub(x, y);
+        D<A> Signum(D<A> x)      => NumDA<A, FracA>.Signum(x);
+        D<A> Abs(D<A> x)         => NumDA<A, FracA>.Abs(x);
+
+        // Implementation of Fractional
+        D<A> FromRational(Ratio<int> x) => D<A>.Const(FromRational(x));
+
+        D<A> Div(D<A> x, D<A> y)
+            => new D<A>(
+                   // Quotient rule
+                   Div(x.X, y.X),
+                   Div(Sub(Mul(x.DX, y.X), Mul(y.DX, x.X)), Mul(y.X, y.X))
+               );
+    }
+
+    instance FloatingDA<A> : Floating<D<A>>
+        where FloatA : Floating<A>
+    {
+        // Implementation of Num
+        D<A> FromInteger(int x)  => FractionalDA<A, FloatA>.FromInteger(x);
+        D<A> Add(D<A> x, D<A> y) => FractionalDA<A, FloatA>.Add(x, y);
+        D<A> Mul(D<A> x, D<A> y) => FractionalDA<A, FloatA>.Mul(x, y);
+        D<A> Sub(D<A> x, D<A> y) => FractionalDA<A, FloatA>.Sub(x, y);
+        D<A> Signum(D<A> x)      => FractionalDA<A, FloatA>.Signum(x);
+        D<A> Abs(D<A> x)         => FractionalDA<A, FloatA>.Abs(x);
+
+        // Implementation of Fractional
+        D<A> FromRational(Ratio<int> x)
+            => FractionalDA<A, FloatA>.FromRational(x);
+        D<A> Div(D<A> x, D<A> y)
+            => FractionalDA<A, FloatA>.Div(x, y);
+
+        // Implementation of Floating
+        D<A> Pi() => D<A>.Const(Pi());
+
+        // d(e^x) = e^x
+        D<A> Exp(D<A> x) => D<A>.Chain(Exp, Exp)(x);
+
+        // d(ln x) = 1/x
+        D<A> Log(D<A> x) => D<A>.Chain(Log, Recip)(x);
+
+        // d(sqrt x) = 1/(2 sqrt x)
+        D<A> Sqrt(D<A> x)
+            => D<A>.Chain(
+                   Sqrt,
+                   Recip(FloatF<A, A, FloatA>.Mul(Two<Func<A, A>>(), Sqrt))
+               )(x);
+
+        // d(x^y) rewrites to D(e^(ln x * y))
+        D<A> Pow(D<A> x, D<A> y) => this.Exp(this.Mul(this.Log(x), y));
+
+        // d(log b(x)) rewrites to D(log x / log b)
+        D<A> LogBase(D<A> b, D<A> x) => this.Div(this.Log(x), this.Log(b));
+
+        // d(sin x) = cos x
+        D<A> Sin(D<A> x) => D<A>.Chain(Sin, Cos)(x);
+
+        // d(sin x) = -sin x
+        D<A> Cos(D<A> x)
+            => D<A>.Chain(Cos, Neg<Func<A, A>>(Sin))(x);
+
+        // d(tan x) = 1 + tan^2 x
+        D<A> Tan(D<A> x)
+            => D<A>.Chain(
+                   Tan,
+                   FloatF<A, A, FloatA>.Add(
+                       One<Func<A, A>>(), Square<Func<A, A>>(Tan)
+                   )
+               )(x);
+
+        // d(asin x) = 1/sqrt(1 - x^2)
+        D<A> Asin(D<A> x)
+            => D<A>.Chain(
+                   Asin,
+                   Recip(
+                       FloatF<A, A, FloatA>.Sqrt(
+                           NumF<A, A, FloatA>.Sub(One<Func<A, A>>(), Square)
+                       )
+                   )
+               )(x);
+
+        // d(acos x) = -1/sqrt(1 - x^2)
+        D<A> Acos(D<A> x)
+            => D<A>.Chain(
+                   Acos,
+                   Recip(
+                       Neg(
+                           FloatF<A, A, FloatA>.Sqrt(
+                               NumF<A, A, FloatA>.Sub(One<Func<A, A>>(), Square)
+                           )
+                       )
+                   )
+               )(x);
+
+        // d(atan x) = 1/(1 + x^2)
+        D<A> Atan(D<A> x)
+            => D<A>.Chain(
+                   Atan,
+                   Recip(NumF<A, A, FloatA>.Add(One<Func<A, A>>(), Square))
+               )(x);
+
+        // d(sinh x) = cosh x
+        D<A> Sinh(D<A> x) => D<A>.Chain(Sinh, Cosh)(x);
+
+        // d(cosh x) = sinh x
+        D<A> Cosh(D<A> x) => D<A>.Chain(Cosh, Sinh)(x);
+
+        // d(tanh x) = 1/(cosh^2 x)
+        D<A> Tanh(D<A> x)
+            => D<A>.Chain(Tanh, Recip(Square<Func<A, A>>(Cosh)))(x);
+
+        // d(asinh x) = 1 / sqrt(x^2 + 1)
+        D<A> Asinh(D<A> x)
+            => D<A>.Chain(
+                   Asinh,
+                   Recip(
+                       FloatF<A, A, FloatA>.Sqrt(
+                           NumF<A, A, FloatA>.Add(Square, One<Func<A, A>>())
+                       )
+                   )
+               )(x);
+
+        // d(acosh x) = 1 / sqrt(x^2 - 1)
+        D<A> Acosh(D<A> x)
+            => D<A>.Chain(
+                   Acosh,
+                   Recip(
+                       FloatF<A, A, FloatA>.Sqrt(
+                           NumF<A, A, FloatA>.Sub(Square, One<Func<A, A>>())
+                       )
+                   )
+               )(x);
+
+        // d(atanh x) = 1 / (1 - x^2)
+        D<A> Atanh(D<A> x)
+            => D<A>.Chain(
+                   Atanh,
+                   Recip(NumF<A, A, FloatA>.Sub(One<Func<A, A>>(), Square))
+               )(x);
+    }
+}
