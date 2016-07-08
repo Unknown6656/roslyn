0fe06666fbb111405a4cecfe6a4c6514f95f6439
Matthew Windsor <t-mawind@microsoft.com>
2016-07-08 15:33:53 +0100

    Add first go at NBE.
    I have no idea how this works...

--- concepts/code/ExpressionUtils/ExpressionUtils/ExpressionUtils.cs
+++ concepts/code/ExpressionUtils/ExpressionUtils/ExpressionUtils.cs
@@ -12,90 +12,108 @@ namespace ExpressionUtils
 {
     using static Utils;
 
-   
+
 
     using Env = Func<Exp, ParameterExpression>;
-    using Map = Func<Exp,Exp>;
+    using Map = Func<Exp, Exp>;
 
-  
-    public abstract class Exp {
+
+    public abstract class Exp
+    {
 
     }
 
-    public abstract class Exp<T> : Exp {
+    public abstract class Exp<T> : Exp
+    {
 
         public abstract Expression Translate(Env E);
 
-        public Func<T> Compile() {
+        public Func<T> Compile()
+        {
             var c = this.Translate(Empty);
             var l = Expression.Lambda<Func<T>>(c);
             return l.Compile();
         }
 
-        public T Run () => Compile()();
+        public T Run() => Compile()();
 
         public virtual Exp<T> Reduce(Map M) { return this; }
     }
 
 
-    public class Constant<T> : Exp<T> {
+    public class Constant<T> : Exp<T>
+    {
         public T c;
-        public Constant(T c) {
+        public Constant(T c)
+        {
             this.c = c;
         }
 
-        public override Expression Translate(Env E) {
+        public override Expression Translate(Env E)
+        {
             return Expression.Constant(c);
         }
     }
-    public class Var<T> : Exp<T> {
-        public Var() {
+    public class Var<T> : Exp<T>
+    {
+        public Var()
+        {
 
         }
 
-        public override Expression Translate(Env E) {
-            return  E(this);
+        public override Expression Translate(Env E)
+        {
+            return E(this);
         }
 
-        public override Exp<T> Reduce(Map M) {
+        public override Exp<T> Reduce(Map M)
+        {
             return (Exp<T>)M(this);
         }
     }
 
-    public class Lam<T,U> : Exp<Func<T,U>> {
+    public class Lam<T, U> : Exp<Func<T, U>>
+    {
         public Var<T> v;
         public Exp<U> e;
-        public Lam(Func<Var<T>,Exp<U>> f) {
+        public Lam(Func<Var<T>, Exp<U>> f)
+        {
             this.v = new Var<T>();
             this.e = f(v);
         }
 
-        public override Expression Translate(Env E) {
+        public override Expression Translate(Env E)
+        {
             var p = Expression.Parameter(typeof(T));
             var Ex = E.Add(v, p);
             return Expression.Lambda(e.Translate(Ex), p);
         }
 
-        public override Exp<Func<T,U>> Reduce(Map M) {
-            return Lam<T,U>(x => e.Reduce(M.Add(v, x)));
+        public override Exp<Func<T, U>> Reduce(Map M)
+        {
+            return Lam<T, U>(x => e.Reduce(M.Add(v, x)));
         }
 
     }
 
-    public class App<T,U> : Exp<U> {
+    public class App<T, U> : Exp<U>
+    {
         public Exp<Func<T, U>> f;
         public Exp<T> e;
-        public App(Exp<Func<T,U>> f, Exp<T> e) {
+        public App(Exp<Func<T, U>> f, Exp<T> e)
+        {
             this.f = f;
             this.e = e;
         }
 
-        public override Expression Translate(Env E) {
+        public override Expression Translate(Env E)
+        {
             return Expression.Invoke(f.Translate(E), e.Translate(E));
         }
 
 
-        public override Exp<U> Reduce(Map M) {
+        public override Exp<U> Reduce(Map M)
+        {
             var fr = f.Reduce(M) as Exp<Func<T, U>>;
             var er = e.Reduce(M) as Exp<T>;
             var lambda = fr as Lam<T, U>;
@@ -107,25 +125,29 @@ namespace ExpressionUtils
 
 
 
-    public class Let<T, U> : Exp<U> {
+    public class Let<T, U> : Exp<U>
+    {
         public Var<T> x;
         public Exp<T> e;
         public Exp<U> f;
-        public Let(Exp<T> e, Func<Var<T>, Exp<U>> f ) {
+        public Let(Exp<T> e, Func<Var<T>, Exp<U>> f)
+        {
             this.x = new Var<T>();
             this.e = e;
             this.f = f(x);
         }
 
-        public override Expression Translate(Env E) {
+        public override Expression Translate(Env E)
+        {
             var p = Expression.Parameter(typeof(T));
             var ce = e.Translate(E);
             var Ex = E.Add(x, p);
             var fc = f.Translate(Ex);
-            return Expression.Block(new [] { p }, Expression.Assign(p, e.Translate(E)),fc);           
+            return Expression.Block(new[] { p }, Expression.Assign(p, e.Translate(E)), fc);
         }
 
-        public override Exp<U> Reduce(Map M) {
+        public override Exp<U> Reduce(Map M)
+        {
             return Let(e.Reduce(M), y => f.Reduce(M.Add(x, y)));
         }
 
@@ -133,17 +155,20 @@ namespace ExpressionUtils
     }
 
 
-    public class Prim<T1, T> : Exp<T> {
+    public class Prim<T1, T> : Exp<T>
+    {
 
         public Expression<Func<T1, T>> f;
         public Exp<T1> e1;
-       
-        public Prim(Expression<Func<T1, T>> f, Exp<T1> e1) {
+
+        public Prim(Expression<Func<T1, T>> f, Exp<T1> e1)
+        {
             this.f = f;
             this.e1 = e1;
         }
 
-        public override Expression Translate(Env E) {
+        public override Expression Translate(Env E)
+        {
             var p = f.Parameters[0];
             var c1 = e1.Translate(E);
             return Expression.Block(new[] { p },
@@ -151,41 +176,47 @@ namespace ExpressionUtils
                                     f.Body);
         }
 
-        public override Exp<T> Reduce(Map M) {
+        public override Exp<T> Reduce(Map M)
+        {
             return Prim(f, e1.Reduce(M));
         }
     }
 
-    public class Prim<T1,T2,T> : Exp<T> {
+    public class Prim<T1, T2, T> : Exp<T>
+    {
 
         public Expression<Func<T1, T2, T>> f;
         public Exp<T1> e1;
         public Exp<T2> e2;
-        public Prim(Expression<Func<T1, T2, T>> f, Exp<T1> e1, Exp<T2> e2) {
+        public Prim(Expression<Func<T1, T2, T>> f, Exp<T1> e1, Exp<T2> e2)
+        {
             this.f = f;
             this.e1 = e1;
             this.e2 = e2;
         }
 
-        public override Expression Translate(Env E) {
+        public override Expression Translate(Env E)
+        {
             var p = f.Parameters[0];
             var q = f.Parameters[1];
             var c1 = e1.Translate(E);
             var c2 = e2.Translate(E);
-            return Expression.Block(new[] { p, q }, 
+            return Expression.Block(new[] { p, q },
                                     Expression.Assign(p, c1),
                                     Expression.Assign(q, c2),
                                     f.Body);
         }
 
-        public override Exp<T> Reduce(Map M) {
-            return Prim(f, e1.Reduce(M),e2.Reduce(M));
+        public override Exp<T> Reduce(Map M)
+        {
+            return Prim(f, e1.Reduce(M), e2.Reduce(M));
         }
     }
 
 
 
-    public static class Utils {
+    public static class Utils
+    {
 
         public static Env Empty = x => { throw new System.ArgumentOutOfRangeException(); };
         public static Map EmptyMap = x => x;
@@ -197,38 +228,79 @@ namespace ExpressionUtils
                      (y) => (x == y) ? p : E(x);
 
         public static Exp<T> C<T>(T t) => new Constant<T>(t);
-        public static Exp<Func<T,U>> Lam<T,U>(Func<Var<T>, Exp<U>> f) =>
-               new Lam<T,U>(f);
+        public static Exp<Func<T, U>> Lam<T, U>(Func<Var<T>, Exp<U>> f) =>
+               new Lam<T, U>(f);
 
 
         public static Exp<U> Let<T, U>(Exp<T> e, Func<Var<T>, Exp<U>> f) =>
-               (e is Var<T>) ? 
-                 f(e as Var<T>) 
+               (e is Var<T>) ?
+                 f(e as Var<T>)
                : new Let<T, U>(e, f);
 
         public static Exp<U> Apply<T, U>(this Exp<Func<T, U>> f, Exp<T> e) =>
               new App<T, U>(f, e);
 
-        public static Exp<T> Prim<T1,T2,T>(Expression<Func<T1, T2, T>> f, Exp<T1> e1, Exp<T2> e2) =>
-             new Prim<T1,T2,T>(f, e1, e2);
+        public static Exp<T> Prim<T1, T2, T>(Expression<Func<T1, T2, T>> f, Exp<T1> e1, Exp<T2> e2) =>
+             new Prim<T1, T2, T>(f, e1, e2);
         public static Exp<T> Prim<T1, T>(Expression<Func<T1, T>> f, Exp<T1> e1) =>
-             new Prim<T1,T>(f, e1);
+             new Prim<T1, T>(f, e1);
 
 
         public static Exp<Func<T, V>> Compose<T, U, V>(Exp<Func<U, V>> f, Exp<Func<T, U>> g) => Lam<T, V>(x => f.Apply(g.Apply(x)));
 
-        public static Exp<Func<T, T>> Pow<T>(Exp<Func<T, T>> f, int n) => (n > 0) ? Compose(f, Pow(f, n - 1)) : Lam<T,T>( x => x);
+        public static Exp<Func<T, T>> Pow<T>(Exp<Func<T, T>> f, int n) => (n > 0) ? Compose(f, Pow(f, n - 1)) : Lam<T, T>(x => x);
 
     }
 
+    public class Coerce<T> : Exp<T>
+    {
+        private readonly Exp<Exp<T>> inner;
 
-    public static class Test {
-        static void Main() {
+        public Coerce(Exp<Exp<T>> e)
+        {
+            inner = e;
+        }
+
+        public override Expression Translate(Env E)
+        {
+            return inner.Run().Translate(E);
+        }
+    }
+
+    public concept Nbe<A>
+    {
+        Exp<A> Reify(A a);
+        A Reflect(Exp<A> ea);
+    }
+
+    public instance NbeFunc<A, B> : Nbe<Func<A, B>>
+        where NbeA : Nbe<A>
+        where NbeB : Nbe<B>
+    {
+        Exp<Func<A, B>> Reify(Func<A, B> a) => Lam<A, B>(x => Reify(a(Reflect(x))));
+        Func<A, B> Reflect(Exp<Func<A, B>> a) => x => Reflect(a.Apply(Reify(x)));
+    }
+
+    public static class NbeUtils
+    {
+        static Exp<A> Nbe<A>(Exp<A> a) where NbeA : Nbe<A> => Reify(a.Run());
+    }
+
+    public instance NbeExp<T> : Nbe<Exp<T>>
+    {
+        Exp<Exp<T>> Reify(Exp<T> e) => new Constant<Exp<T>>(e);
+        Exp<T> Reflect(Exp<Exp<T>> e) => new Coerce<T>(e);
+    }
+
+    public static class Test
+    {
+        static void Main()
+        {
 
             var t1 = C(1);
             var r1 = t1.Run();
 
-            var t2 = Lam<int,int>(x => x);
+            var t2 = Lam<int, int>(x => x);
             var r2 = t2.Run();
 
             var t3 = t2.Apply(t1);
@@ -239,11 +311,11 @@ namespace ExpressionUtils
             var t4 = Let(t1, x => x);
             var r4 = t4.Run();
 
-            var t6 = Let(t1, x =>  t2.Apply(x));
+            var t6 = Let(t1, x => t2.Apply(x));
             var r6 = t6.Run();
 
 
-            var t7 = Prim(x=>Math.Cos(x),C(0.0) );
+            var t7 = Prim(x => Math.Cos(x), C(0.0));
 
             var r7 = t7.Run();
 
@@ -263,7 +335,7 @@ namespace ExpressionUtils
             var r10opt = t10.Reduce(EmptyMap).Run();
 
 
-            
+
             System.Console.WriteLine(r10opt(100));
 
 
