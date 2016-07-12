e12ce29d6ff832a539d5016ed80a7e27bac63957
Matthew Windsor <t-mawind@microsoft.com>
2016-07-12 14:20:34 +0100

    Add defaults testbed code.

---
+++ concepts/code/DefaultsTestbed/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Concepts;
+
+namespace DefaultsTestbed
+{
+    class Program
+    {
+        concept Eq<A>
+        {
+            bool Eq(A a, A b);
+            bool Neq(A a, A b);
+        }
+
+        [ConceptDefault]
+        struct Eq_default<A> where EqA : Eq<A>
+        {
+            public bool Eq(A a, A b) => !EqA.Neq(a, b);
+            public bool Neq(A a, A b) => !EqA.Eq(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
