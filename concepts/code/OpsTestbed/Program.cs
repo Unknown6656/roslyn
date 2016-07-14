cfb15dc2b48ff1fa2c1739b025a23330b4124c34
Matthew Windsor <t-mawind@microsoft.com>
2016-07-14 17:53:21 +0100

    Add new perf examples and change config.

---
+++ concepts/code/OpsTestbed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Concepts.OpPrelude;
+
+namespace OpsTestbed
+{
+    class Program
+    {
+        static A M<A>(A x) where NumA : Num<A> => FromInteger(666) * x * x * x + FromInteger(777) * x * x + FromInteger(888);
+
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debugger.Launch();
+            System.Diagnostics.Debugger.Break();
+
+            Console.WriteLine(M(255)); // int
+            Console.WriteLine(M(255.0)); // double
+        }
+    }
+}
+
