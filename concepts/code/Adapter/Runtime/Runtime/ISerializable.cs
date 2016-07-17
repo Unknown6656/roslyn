dd7e705b2e852d728b0a3bb305f6754bf8a0545e
Claudio Russo <crusso@hotmail.co.uk>
2016-07-17 19:02:40 +0100

    formatting

--- concepts/code/Adapter/Runtime/Runtime/ISerializable.cs
+++ concepts/code/Adapter/Runtime/Runtime/ISerializable.cs
@@ -1,19 +1,12 @@
-﻿namespace Runtime
-{
-    public concept ISerializable<T>
-    {
+﻿namespace Runtime {
+    public concept ISerializable<T>  {
         string Serialize(T t);
         T DeSerialize(string t);
     }
-
-    public static class Extensions
-    {
+    public static class Extensions {
         public static string Serialize<T>(this T This) where E : ISerializable<T>
             => E.Serialize(This);
-
-
         public static T DeSerialize<T>(this string This) where E : ISerializable<T>
            => E.DeSerialize(This);
-
     }
 }
