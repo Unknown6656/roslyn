5c27851ebe2ad86d8d7e78f2be9321031f31ff13
Matthew Windsor <t-mawind@microsoft.com>
2016-08-12 12:02:06 +0100

    Split CEnumerable.

--- concepts/code/AssociatedTypes/Enumerable.cs
+++ concepts/code/AssociatedTypes/Enumerable.cs
@@ -240,13 +240,29 @@ namespace AssociatedTypes
     /// <typeparam name="S">
     ///     The state held by the enumerator.
     /// </typeparam>
-    public concept CEnumerable<C, [AssociatedType] E, [AssociatedType] S>
+    public concept CEnumerator<E, [AssociatedType] S>
     {
-        S GetEnumerator(C container);
         void Reset(ref S enumerator);
         bool MoveNext(ref S enumerator);
         E Current(ref S enumerator);
         void Dispose(ref S enumerator);
+    }
+
+    /// <summary>
+    ///     Concept for types which may be enumerated.
+    /// </summary>
+    /// <typeparam name="C">
+    ///     The type to be enumerated.
+    /// </typeparam>
+    /// <typeparam name="E">
+    ///     The element returned by the enumerator.
+    /// </typeparam>
+    /// <typeparam name="S">
+    ///     The state held by the enumerator.
+    /// </typeparam>
+    public concept CEnumerable<C, [AssociatedType] E, [AssociatedType] S> : CEnumerator<E, S>
+    {
+        S GetEnumerator(C container);
     }
 
 
