89a8f5708e86267bc02f9a54b79597adda8a6af1
Claudio Russo <crusso@hotmail.co.uk>
2017-01-05 19:51:43 +0000

    revised slides

---
+++ concepts/docs/Make.bat
@@ -0,0 +1,4 @@
+call C:\nodejs\nodevars.bat
+REM npm install markdown-to-html -g
+REM markdown-to-slides -l "template\\template.html"  -i fsconcepts.md -o fsconcepts.html
+markdown-to-slides -l "template\\template.html"  -i csconcepts.md -o csconcepts.html
